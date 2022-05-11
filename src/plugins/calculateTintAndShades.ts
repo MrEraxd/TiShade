64c4d7133d91d92f69270d18f61616c4530d08b0
MrEraxd <MrEraxd@gmail.com>
2022-05-11 21:56:35 +0200

    Added lightess by mixing

--- src/plugins/calculateTintAndShades.ts
+++ src/plugins/calculateTintAndShades.ts
@@ -1,17 +1,12 @@
-import {
-  hexToHsl,
-  lightenColor,
-  darkenColor,
-  hexToRgb,
-  hslToRgb,
-  hslToHex,
-} from './colorFunctions';
+import { colorFunctions as cf } from './colorFunctions';
 import { IColor } from 'interfaces/Color';
 
 export const calculateTintsAndShades = (
   hexColor: string,
   lightenByMixing = false
 ): IColor[] | undefined => {
+  const colors = new cf();
+
   // If not hex color
   if (hexColor.search(/[0-9A-Fa-f]{6}/g) === -1) {
     console.error('Wrong color format');
@@ -19,8 +14,8 @@ export const calculateTintsAndShades = (
   }
 
   const baseColor: IColor = {
-    rgb: hexToRgb(hexColor),
-    hsl: hexToHsl(hexColor),
+    rgb: colors.hexToRgb(hexColor),
+    hsl: colors.hexToHsl(hexColor),
     hex: {
       r: hexColor[1] + hexColor[2],
       g: hexColor[3] + hexColor[4],
@@ -32,21 +27,60 @@ export const calculateTintsAndShades = (
   const step = 20;
 
   if (lightenByMixing) {
-    console.log('lightenByMixing');
+    for (let i = 0; i < 100; i++) {
+      const lightestColor = baseColor;
+      const lighterRgbColor = colors.lightenColorByMixing(
+        lightestColor.rgb,
+        0.1
+      );
+
+      if (
+        JSON.stringify(lighterRgbColor) === JSON.stringify(lightestColor.rgb)
+      ) {
+        break;
+      }
+
+      colorArray.push({
+        rgb: lighterRgbColor,
+        hex: colors.rgbToHex(lighterRgbColor),
+        hsl: colors.rgbToHsl(lighterRgbColor),
+      });
+    }
+
+    for (let i = 0; i < 100; i++) {
+      const darkestColor = baseColor;
+      const darkerRgbColor = colors.darkenColorByMixing(
+        darkestColor.rgb,
+        0.1 * i
+      );
+
+      if (JSON.stringify(darkerRgbColor) === JSON.stringify(darkestColor.rgb)) {
+        break;
+      }
+
+      colorArray.unshift({
+        rgb: darkerRgbColor,
+        hex: colors.rgbToHex(darkerRgbColor),
+        hsl: colors.rgbToHsl(darkerRgbColor),
+      });
+    }
   } else {
     const startingLightnes = colorArray[0].hsl.l;
     // Lighter colors
     for (let i = 0; i < 100 - startingLightnes; i += step) {
       const lightestColor = colorArray[colorArray.length - 1];
-      const lighterHslColor = lightenColor(lightestColor.hsl, step);
+      const lighterHslColor = colors.lightenColorByLightness(
+        lightestColor.hsl,
+        step
+      );
 
       if (lighterHslColor.l === lightestColor.hsl.l) {
         break;
       }
 
       colorArray.push({
-        rgb: hslToRgb(lighterHslColor),
-        hex: hslToHex(lighterHslColor),
+        rgb: colors.hslToRgb(lighterHslColor),
+        hex: colors.hslToHex(lighterHslColor),
         hsl: lighterHslColor,
       });
     }
@@ -54,15 +88,18 @@ export const calculateTintsAndShades = (
     // Darker colors
     for (let i = 0; i < startingLightnes; i += step) {
       const darkestColor = colorArray[0];
-      const darkerHslColor = darkenColor(darkestColor.hsl, step);
+      const darkerHslColor = colors.darkenColorByLightness(
+        darkestColor.hsl,
+        step
+      );
 
       if (darkerHslColor.l === darkestColor.hsl.l) {
         break;
       }
 
       colorArray.unshift({
-        rgb: hslToRgb(darkerHslColor),
-        hex: hslToHex(darkerHslColor),
+        rgb: colors.hslToRgb(darkerHslColor),
+        hex: colors.hslToHex(darkerHslColor),
         hsl: darkerHslColor,
       });
     }
