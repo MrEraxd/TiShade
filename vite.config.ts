5af225140d18808a481ed31af6aa10ae1d1347dc
MrEraxd <MrEraxd@gmail.com>
2022-05-18 22:39:10 +0200

    Added store alias

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,10 @@ export default defineConfig({
         find: '@interfaces',
         replacement: path.resolve(path.dirname(''), 'src/interfaces'),
       },
+      {
+        find: '@store',
+        replacement: path.resolve(path.dirname(''), 'src/stores'),
+      },
     ],
   },
 });
